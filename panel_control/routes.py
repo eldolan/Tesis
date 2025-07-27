ec3a7a1afdce6eb6d32bf5d4ec64e86783249266
Dylan <d.moenadelgadillo@uandresbello.edu>
2025-07-27 01:32:51 -0400

    Revert commit anterior

--- panel_control/routes.py
+++ panel_control/routes.py
@@ -6,12 +6,11 @@ def home():
     return render_template('index.html')
 
 from . import db
-from sqlalchemy import text
 
 @app.route('/test-db')
 def test_db_connection():
     try:
-        db.session.execute(text('SELECT 1'))
+        db.session.execute('SELECT 1')
         return "Conexión a la base de datos exitosa!"
     except Exception as e:
         return f"Error al conectar a la base de datos: {e}"
