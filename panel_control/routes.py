038fc6246bda7d4696a5102bf1b1d6258fa2884a
Dylan <d.moenadelgadillo@uandresbello.edu>
2025-07-27 00:05:26 -0400

    Test de conexion con BD

--- panel_control/routes.py
+++ panel_control/routes.py
@@ -4,3 +4,13 @@ from flask import current_app as app
 @app.route('/')
 def home():
     return render_template('index.html')
+
+from . import db
+
+@app.route('/test-db')
+def test_db_connection():
+    try:
+        db.session.execute('SELECT 1')
+        return "Conexión a la base de datos exitosa!"
+    except Exception as e:
+        return f"Error al conectar a la base de datos: {e}"
