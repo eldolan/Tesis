1957cfefc1076eccc5c66068d138b768d29133fd
Dylan <d.moenadelgadillo@uandresbello.edu>
2025-07-31 00:23:59 -0400

    Avances navbar

---
+++ panel_control/static/js/script.js
@@ -0,0 +1,9 @@
+const navbar = document.getElementById('navbar');
+
+function abrirSidebar() {
+    navbar.classList.add('show');
+}
+
+function cerrarSidebar() {
+    navbar.classList.remove('show');
+}
